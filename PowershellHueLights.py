6cc11365d3f95abbfc72af4ce9aeb6ed93a5d476
cammoulder <cameronmoulder@gmail.com>
2017-02-13 20:46:05 -0500

    Update PowershellHueLights.py

--- PowershellHueLights.py
+++ PowershellHueLights.py
@@ -6,7 +6,7 @@ $UserID = "21xKFR5AZpeqZByyBUHTD2lcdxXui17j2aoWbhwM" # API "key" / password / us
 # Instantiate the class and assign to the $Office variable
 $bedroom = [HueLight]::new("cams", $Endpoint, $UserID)
 
-# If the light isn't already on, turn it on.
+# If the light isn't already on, turn it on and make them green/white
 If ($bedroom.On -ne $true) {
     $bedroom.SwitchHueLight("on");
     $bedroom.SetHueLight(254,22000,50)
